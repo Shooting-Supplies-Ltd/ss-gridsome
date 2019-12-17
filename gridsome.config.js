aa59b79ef810c5a6bed7ab30a3cd5deafeaad87b
Darryl Morley <darrylmorley.uk@gmail.com>
2019-12-17 17:14:53 +0000

    Remove site breaking changes

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,12 +4,6 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-Za-z0-9-_:\/]+/g) || []
-  }
-}
-
 module.exports = {
   siteName: 'Shooting Supplies Ltd - Guns',
   siteDescription: "Shooting Supplies Ltd are the Midlands leading Firearms Dealer & Shooting Supplies Store. Your one-stop-shop for all of your shooting needs.",
@@ -40,41 +34,6 @@ module.exports = {
       }
     }
   ],
-  chainWebpack: config => {
-    config.module
-      .rule('css')
-      .oneOf('normal')
-      .use('postcss-loader')
-      .tap(options => {
-        options.plugins.unshift(...[
-          require('postcss-import'),
-          require('postcss-nested'),
-          require('tailwindcss'),
-        ])
-
-        if (process.env.NODE_ENV === 'production') {
-          options.plugins.push(...[
-            require('@fullhuman/postcss-purgecss')({
-              content: [
-                'src/assets/**/*.css',
-                'src/**/*.vue',
-                'src/**/*.js'
-              ],
-              extractors: [
-                {
-                  extractor: TailwindExtractor,
-                  extensions: ['css', 'vue', 'js']
-                }
-              ],
-              whitelist: ['svg-inline--fa'],
-              whitelistPatterns: [/shiki/, /fa-$/]
-            })
-          ])
-        }
-        return options
-      })
-  }
-
 }
 
   
