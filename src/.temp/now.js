9cefb3764fc4b49c8be0df42f08614d6af2cea0f
Darryl Morley <darrylmorley.uk@gmail.com>
2019-12-17 17:05:42 +0000

    All changes

--- src/.temp/now.js
+++ src/.temp/now.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export default 1576601385572
-=======
-export default 1576591198730
->>>>>>> parent of 4788fb8... Remove text sizing on elements with font awesome icons
+export default 1576602179529
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,23 @@
 <template>
-<<<<<<< HEAD
     <header class="w-full shadow-lg border-t-4 mb-8 border-ssblue">
         <div id="top-bar" class=" bg-ssblue w-full h-8 flex">
-=======
-    <header class="w-full shadow-lg border-t-4 mb-8 rounded-t border-ssblue">
-        <!-- <div id="top-bar" class=" bg-ssblue w-full h-8 flex">
->>>>>>> parent of 4788fb8... Remove text sizing on elements with font awesome icons
         
-        <div class="container w-1/2 flex flex-initial justify-center items-center text-white">
+        <div class="container w-1/2 flex justify-center items-center text-white">
           <div id="contact" class="flex items-center">
-            <p class="mr-4"><a href="tel:01527831261"><span class="hover:text-ssorange">Call Us: 0152783126<font-awesome :icon="['fas', 'phone']" size="sm" class="ml-2"/></span></a></p>
-            <p><a href="mailto:info@shootingsuppliesltd.co.uk"><span class="hover:text-ssorange">Email Us: <font-awesome :icon="['fas', 'envelope']" size="sm" class="ml-2"/></span></a></p>
+            <p class="mr-8"><a href="tel:01527831261"><span class="hover:text-ssorange">Call Us: 0152783126<font-awesome :icon="['fas', 'phone']" class="ml-2"/></span></a></p>
+            <p><a href="mailto:info@shootingsuppliesltd.co.uk"><span class="hover:text-ssorange">Email Us: <font-awesome :icon="['fas', 'envelope']" class="ml-2"/></span></a></p>
           </div>  
         </div>
         
-        <div class="container w-1/2 flex flex-initial justify-center items-center text-white">
+        <div class="container w-1/2 flex justify-center items-center text-white">
           <div id="social" class="flex items-center">
             <p class="mr-4">Find Us On:</p>
-            <p class="text-xl mr-4"><a href="https://www.facebook.com/ShootingSuppliesLTD/" target="_blank"><font-awesome :icon="['fab', 'facebook-square']" size="sm" class="hover:text-ssorange" /></a></p>
-            <p class="text-xl mr-4"><a href="https://twitter.com/shootingsupplys" target="_blank"><font-awesome :icon="['fab', 'twitter']" size="sm" class="hover:text-ssorange" /></a></p>
-            <p class="text-xl"><a href="https://www.instagram.com/shootingsupplies/" target="_blank"><font-awesome :icon="['fab', 'instagram']" size="sm" class="hover:text-ssorange" /></a></p>
+            <p class="mr-4"><a href="https://www.facebook.com/ShootingSuppliesLTD/" target="_blank"><font-awesome :icon="['fab', 'facebook-square']" class="hover:text-ssorange" /></a></p>
+            <p class="mr-4"><a href="https://twitter.com/shootingsupplys" target="_blank"><font-awesome :icon="['fab', 'twitter']" class="hover:text-ssorange" /></a></p>
+            <p class=""><a href="https://www.instagram.com/shootingsupplies/" target="_blank"><font-awesome :icon="['fab', 'instagram']" class="hover:text-ssorange" /></a></p>
           </div>
         </div>
-      </div> -->
+      </div>
        <div class="flex justify-center bg-white">
         <h1 class="hidden">Shooting Supplies Ltd / Guns</h1>
         <g-link to="/"><g-image class="block flex items-center max-w-xl my-4"
@@ -32,8 +27,8 @@
 
       <nav id="menu" class="w-full flex justify-around items-center bg-ssblue text-white uppercase h-12 font-semibold text-lg">
           <div class="flex">
-          <a href="https://shootingsuppliesltd.co.uk"></a><p class="mr-12 hover:text-ssorange">Home</p>
-          <g-link to="/"><p class="mr-12 hover:text-ssorange">Guns</p></g-link>
+          <a href="https://shootingsuppliesltd.co.uk"><p class="mr-12 hover:text-ssorange">Home</p></a>
+          <a href="https://shootingsuppliesltd.co.uk"><p class="mr-12 hover:text-ssorange">Guns</p></a>
           <a href="https://shootingsuppliesltd.co.uk/product-category/optics/"><p class="mr-12 hover:text-ssorange">Optics</p></a>
           <a href="https://shootingsuppliesltd.co.uk/ammunition/"><p class="mr-12 hover:text-ssorange">Ammunition</p></a>
           <a href="https://shootingsuppliesltd.co.uk/clothing-footwear/"><p class="mr-12 hover:text-ssorange">Clothing</p></a>
