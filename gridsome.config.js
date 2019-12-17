698b89f338e3a615c5a3a68213070a180c5e0560
Darryl Morley <darrylmorley.uk@gmail.com>
2019-12-17 10:32:43 +0000

    Remove all Wordpress

--- gridsome.config.js
+++ gridsome.config.js
@@ -9,16 +9,6 @@ module.exports = {
   siteDescription: "Your one stop shop for guns of all types, both new & used.",
   plugins: [
     {
-      use: '@gridsome/source-wordpress',
-      options: {
-        baseUrl: 'https://shootingsuppliesltd.co.uk', // required
-        apiBase: 'wp-json',
-        typeName: 'WordPress',
-        perPage: 100,
-        concurrent: 10
-      }
-    },
-    {
       use: 'gridsome-plugin-tailwindcss',
       options: {
         tailwindConfig: '',
@@ -44,11 +34,6 @@ module.exports = {
       }
     }
   ],
-  templates: {
-    WordPressCategory: '/blog/category/:slug', // adds route for "category" post type (Optional)
-    WordPressPost: '/blog/:year/:month/:day/:slug', //adds route for "post" post type (Optional)
-    WordPressPostTag: '/blog/tag/:slug' // adds route for "post_tag" post type (Optional)
-  },
 }
 
   
